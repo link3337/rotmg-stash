3a7954c358a91d6fe63acbce7e84280dc43edbb7
link3337 <marcel.graf33@gmail.com>
2025-03-17 02:33:18 +0100

    feat: add dialog plugin and enhance access token handling in RotMG API integration

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,6 +3,7 @@
 
 mod util;
 
+use base64::{engine::general_purpose, Engine as _};
 use dirs;
 use log;
 use regex::Regex;
@@ -10,6 +11,7 @@ use reqwest::{header, Client};
 use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::PathBuf;
+use std::process::Command;
 use std::time::Instant;
 use tauri_plugin_log;
 use url::form_urlencoded;
@@ -20,6 +22,13 @@ enum AccountError {
     TokenNotFound,
     CouldNotParseToken,
     InvalidResponse(String),
+}
+
+#[derive(Serialize, Deserialize)]
+struct AccessTokenResponse {
+    access_token: String,
+    timestamp: String,
+    expiration: String,
 }
 
 #[derive(Serialize, Deserialize, Default, Debug)]
@@ -38,15 +47,62 @@ impl std::fmt::Display for AccountError {
     }
 }
 
-// credits: https://github.com/faynt0
+/// Launches RotMG.exe
 #[tauri::command]
-async fn get_account(guid: &str, password: &str) -> Result<String, String> {
+async fn launch_exalt(
+    exalt_path: &str,
+    device_token: &str,
+    guid: &str,
+    password: &str,
+) -> Result<String, String> {
+    // Build the path to the executable.
+    let mut exalt_path = PathBuf::from(exalt_path);
+    exalt_path.push("RotMG Exalt.exe");
+
+    log::info!("[Exalt] Launching Exalt at: {:?}", exalt_path);
+
+    // get access token
+    let access_token: AccessTokenResponse = get_access_token(guid, password, Some(device_token))
+        .await
+        .map_err(|e| e.to_string())?;
+
+    // Encode parameters in Base64.
+    let encoded_guid = general_purpose::STANDARD.encode(guid);
+    let encoded_token = general_purpose::STANDARD.encode(access_token.access_token);
+    let encoded_timestamp = general_purpose::STANDARD.encode(access_token.timestamp);
+    let encoded_expiration = general_purpose::STANDARD.encode(access_token.expiration);
+
+    // Build the arguments string.
+    let args = format!(
+        "data:{{platform:Deca,guid:{},token:{},tokenTimestamp:{},tokenExpiration:{},env:4}}",
+        encoded_guid, encoded_token, encoded_timestamp, encoded_expiration
+    );
+
+    // Start the process.
+    Command::new(&exalt_path)
+        .arg(args)
+        .spawn()
+        .map(|_| ())
+        .map_err(|e| e.to_string())?;
+
+    Ok("Done".to_string())
+}
+
+/// Gets an access token from the RotMG API.
+/// This is used to authenticate the user and get the data from the API.
+async fn get_access_token(
+    guid: &str,
+    password: &str,
+    device_token: Option<&str>,
+) -> Result<AccessTokenResponse, String> {
+    let device_token = device_token.unwrap_or("0");
+
     let base_url: &str = "https://www.realmofthemadgod.com";
     let client: Client = Client::new();
 
     let is_steam: bool = guid.starts_with("steamworks:");
 
-    let mut form_data: Vec<(&str, &str)> = vec![("clientToken", "0"), ("guid", guid)];
+    let mut form_data: Vec<(&str, &str)> = vec![("clientToken", device_token), ("guid", guid)];
 
     if is_steam {
         form_data.push(("steamid", guid));
@@ -57,8 +113,6 @@ async fn get_account(guid: &str, password: &str) -> Result<String, String> {
 
     let verify_url: String = format!("{}/account/verify", base_url);
     log::info!("[RotMG API] Sending /account/verify request");
-
-    let start = Instant::now();
 
     let response: reqwest::Response = client
         .post(&verify_url)
@@ -72,33 +126,84 @@ async fn get_account(guid: &str, password: &str) -> Result<String, String> {
 
     log::info!("[RotMG API] Parsing Access Token");
 
-    // get access token
-    let token_regex: Regex = Regex::new(r#"<AccessToken>(.*?)</AccessToken>"#)
+    // Create regex patterns with named capture groups.
+    let token_regex = Regex::new(r#"<AccessToken>(?P<access_token>.*?)</AccessToken>"#)
         .map_err(|e| AccountError::InvalidResponse(e.to_string()).to_string())?;
+    let ts_regex = Regex::new(r#"<AccessTokenTimestamp>(?P<timestamp>.*?)</AccessTokenTimestamp>"#)
+        .map_err(|e| AccountError::InvalidResponse(e.to_string()).to_string())?;
+    let exp_regex =
+        Regex::new(r#"<AccessTokenExpiration>(?P<expiration>.*?)</AccessTokenExpiration>"#)
+            .map_err(|e| AccountError::InvalidResponse(e.to_string()).to_string())?;
 
-    let access_token: String = match token_regex.captures(&body) {
-        Some(caps) => caps.get(1).map(|m| m.as_str().to_string()).ok_or_else(|| {
+    // Capture access token.
+    let token_caps = token_regex.captures(&body).ok_or_else(|| {
+        log::error!(
+            "[RotMG API] Access token not found. Response body: {}",
+            body
+        );
+        AccountError::TokenNotFound.to_string()
+    })?;
+
+    let access_token = token_caps
+        .name("access_token")
+        .map(|m| m.as_str().to_string())
+        .ok_or_else(|| {
             log::error!(
                 "[RotMG API] Failed to parse access token. Response body: {}",
                 body
             );
             AccountError::CouldNotParseToken.to_string()
-        })?,
-        None => {
-            log::error!(
-                "[RotMG API] Access token not found. Response body: {}",
-                body
-            );
-            return Err(AccountError::TokenNotFound.to_string());
-        }
+        })?;
+
+    let ts_caps = ts_regex
+        .captures(&body)
+        .ok_or_else(|| "Access token timestamp not found".to_string())?;
+
+    let timestamp = ts_caps
+        .name("timestamp")
+        .map(|m| m.as_str().to_string())
+        .ok_or_else(|| "Could not parse access token timestamp".to_string())?;
+
+    // Capture expiration.
+    let exp_caps = exp_regex
+        .captures(&body)
+        .ok_or_else(|| "Access token expiration not found".to_string())?;
+
+    let expiration = exp_caps
+        .name("expiration")
+        .map(|m| m.as_str().to_string())
+        .ok_or_else(|| "Could not parse access token expiration".to_string())?;
+
+    // populate model
+    let access_token_response = AccessTokenResponse {
+        access_token,
+        timestamp,
+        expiration,
     };
+
+    Ok(access_token_response)
+}
+
+/// Gets the account data (charlist) from the RotMG API.
+// credits: https://github.com/faynt0
+#[tauri::command]
+async fn get_account_data(guid: &str, password: &str) -> Result<String, String> {
+    let base_url: &str = "https://www.realmofthemadgod.com";
+    let client: Client = Client::new();
+
+    let start = Instant::now();
+
+    // get access token
+    let access_token: AccessTokenResponse = get_access_token(guid, password, None)
+        .await
+        .map_err(|e| e.to_string())?;
 
     // send another request to /charlist
     let char_list_url: String = format!(
         "{}/char/list?muleDump=true&{}",
         base_url,
         form_urlencoded::Serializer::new(String::new())
-            .append_pair("accessToken", &access_token)
+            .append_pair("accessToken", &access_token.access_token)
             .finish()
     );
 
@@ -111,7 +216,9 @@ async fn get_account(guid: &str, password: &str) -> Result<String, String> {
         .map_err(|e| e.to_string())?;
 
     let char_list_body = char_list_response.text().await.map_err(|e| e.to_string())?;
+
     let duration = start.elapsed();
+
     log::info!(
         "[RotMG API] Request completed in {:.2}ms",
         duration.as_secs_f64() * 1000.0
@@ -120,6 +227,8 @@ async fn get_account(guid: &str, password: &str) -> Result<String, String> {
     Ok(char_list_body)
 }
 
+/// Gets the settings from the settings file.
+/// If the file does not exist, it will generate a new one.
 #[tauri::command]
 async fn get_settings() -> Result<Settings, String> {
     let settings_path: PathBuf = get_settings_file_path();
@@ -150,6 +259,7 @@ async fn get_settings() -> Result<Settings, String> {
     Ok(settings)
 }
 
+/// Gets the path to the save file directory.
 fn get_save_file_path() -> PathBuf {
     let path: PathBuf = dirs::data_local_dir()
         .ok_or("Could not get local data directory")
@@ -166,6 +276,7 @@ fn get_save_file_path() -> PathBuf {
     path
 }
 
+/// Gets the path to the settings file.
 fn get_settings_file_path() -> PathBuf {
     let base_path: PathBuf = get_save_file_path();
     log::info!("[Settings] Base path: {:?}", base_path);
@@ -179,6 +290,7 @@ fn get_settings_file_path() -> PathBuf {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(
             tauri_plugin_log::Builder::new()
@@ -191,7 +303,11 @@ pub fn run() {
                 .timezone_strategy(tauri_plugin_log::TimezoneStrategy::UseLocal)
                 .build(),
         )
-        .invoke_handler(tauri::generate_handler![get_account, get_settings])
+        .invoke_handler(tauri::generate_handler![
+            get_account_data,
+            get_settings,
+            launch_exalt
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
